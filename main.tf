45758374d36cae378fe9c421c5019b65088f46c1
Mike Hoskins <mhoskins@pivotal.io>
2019-11-08 12:16:35 -0500

    add vpc and igw with configurable environment name

--- main.tf
+++ main.tf
@@ -4,3 +4,37 @@ provider "aws" {
 
 data "aws_availability_zones" "all" {}
 
+######################################################################
+# Network configuration
+######################################################################
+
+resource "aws_vpc" "vpc" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
+
+  tags = {
+    "Name" = "${var.env_name}-vpc"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    "Name" = "${var.env_name}-igw"
+  }
+}
+
+resource "aws_route_table" "public_route" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    "Name" = "${var.env_name}-public-route"
+  }
+}
+
--- variables.tf
+++ variables.tf
@@ -1,3 +1,8 @@
+variable "env_name" {
+  description = "Short descriptive name to help identify resources we create"
+  type        = "string"
+}
+
 variable "region" {
   type    = "string"
   default = "us-east-2"
