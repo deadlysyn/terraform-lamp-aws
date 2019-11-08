5cc2e8edeabafca8fab225514d68a68b08f4f44c
Mike Hoskins <mhoskins@pivotal.io>
2019-11-08 12:43:52 -0500

    add cidr vars

--- variables.tf
+++ variables.tf
@@ -18,3 +18,18 @@ variable "db_password" {
   type        = "string"
 }
 
+variable "vpc_cidr" {
+  type    = "string"
+  default = "10.1.0.0/16"
+}
+
+variable "public_cidr" {
+  type    = "string"
+  default = "10.1.1.0/24"
+}
+
+variable "private_cidr" {
+  type    = "string"
+  default = "10.1.2.0/24"
+}
+
