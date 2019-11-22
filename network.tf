bbdf096a342cd3bf393b29bbd1738e11b7389b48
Mike Hoskins <mrh@devopsdreams.io>
2019-11-21 21:33:18 -0500

    add nat gateway for private subnets; tweak cidrsubnet bits

--- network.tf
+++ network.tf
@@ -8,8 +8,7 @@ data "aws_availability_zones" "available" {
 }
 
 resource "aws_vpc" "vpc" {
-  cidr_block           = var.vpc_cidr
-  enable_dns_hostnames = true
+  cidr_block = var.vpc_cidr
 
   tags = {
     "Name" = "${var.env_name}-vpc"
@@ -24,18 +23,25 @@ resource "aws_internet_gateway" "igw" {
   }
 }
 
-# TODO: aws_eip / NAT gateway
-#resource "aws_eip" "lb" {
-#  instance = "${aws_instance.web.id}"
-#  vpc      = true
-#}
-#
-#resource "aws_nat_gateway" "ngw" {
-#  count         = length(data.aws_availability_zones.available.names)
-#  subnet_id     = element(aws_subnet.public_subnets[*].id, count.index)
-#  allocation_id = aws_eip.nat.id
-#  depends_on    = ["aws_internet_gateway.igw"]
-#}
+resource "aws_nat_gateway" "ngw" {
+  count         = length(data.aws_availability_zones.available.names)
+  subnet_id     = element(aws_subnet.public_subnets[*].id, count.index)
+  allocation_id = element(aws_eip.nat_eip[*].id, count.index)
+  depends_on    = [aws_internet_gateway.igw]
+
+  tags = {
+    "Name" = "${var.env_name}-ngw-${count.index}"
+  }
+}
+
+resource "aws_eip" "nat_eip" {
+  count      = length(data.aws_availability_zones.available.names)
+  vpc        = true
+  depends_on = [aws_internet_gateway.igw]
+  tags = {
+    "Name" = "${var.env_name}-nat-eip-${count.index}"
+  }
+}
 
 resource "aws_route_table" "public_route" {
   vpc_id = aws_vpc.vpc.id
@@ -50,10 +56,30 @@ resource "aws_route_table" "public_route" {
   }
 }
 
+resource "aws_route_table" "private_route" {
+  count  = length(data.aws_availability_zones.available.names)
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = element(aws_nat_gateway.ngw[*].id, count.index)
+  }
+
+  tags = {
+    "Name" = "${var.env_name}-private-route-${count.index}"
+  }
+}
+
 resource "aws_route_table_association" "public_rta" {
   count          = length(data.aws_availability_zones.available.names)
   subnet_id      = element(aws_subnet.public_subnets[*].id, count.index)
   route_table_id = aws_route_table.public_route.id
+}
+
+resource "aws_route_table_association" "private_rta" {
+  count          = length(data.aws_availability_zones.available.names)
+  subnet_id      = element(aws_subnet.private_subnets[*].id, count.index)
+  route_table_id = element(aws_route_table.private_route[*].id, count.index)
 }
 
 # Important Note:
@@ -61,12 +87,19 @@ resource "aws_route_table_association" "public_rta" {
 # CIDR ranges to satisfy HA and RDS subnet group requirements. However,
 # since all regions do not have a consistent number of AZs, the size of
 # the auto-generated subnets will vary depending on the target region. Be
-# sure to think about scaling requirements when selecting a CIDR range.
+# sure to think about scaling requirements when selecting CIDR ranges.
+
+locals {
+  # When we have 2, 3 or 4 AZs in a region divide the public and private
+  # CIDR ranges into 4 subnets (add 2 bits to netmask). In larger regions
+  # with >4 AZs, divide into 8 subnets (add 3 bits to netmask).
+  newbits = length(data.aws_availability_zones.available.names) > 4 ? 3 : 2
+}
 
 resource "aws_subnet" "public_subnets" {
   count                   = length(data.aws_availability_zones.available.names)
   vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = cidrsubnet(var.public_cidr, length(data.aws_availability_zones.available.names), count.index)
+  cidr_block              = cidrsubnet(var.public_cidr, local.newbits, count.index)
   availability_zone       = element(data.aws_availability_zones.available.names, count.index)
   map_public_ip_on_launch = true
 
@@ -78,7 +111,7 @@ resource "aws_subnet" "public_subnets" {
 resource "aws_subnet" "private_subnets" {
   count             = length(data.aws_availability_zones.available.names)
   vpc_id            = aws_vpc.vpc.id
-  cidr_block        = cidrsubnet(var.private_cidr, length(data.aws_availability_zones.available.names), count.index)
+  cidr_block        = cidrsubnet(var.private_cidr, local.newbits, count.index)
   availability_zone = element(data.aws_availability_zones.available.names, count.index)
 
   tags = {
